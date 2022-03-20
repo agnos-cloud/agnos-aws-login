3dfc1c6f6c6b47c5c101eabd67f7cb2c6f7cf80d
chieze-franklin <chieze.franklin@gmail.com>
2022-03-20 03:47:44 +0100

    implement aws amplify+cognito

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import Amplify from "aws-amplify";
 
 import { withAuthenticator } from "@aws-amplify/ui-react";
@@ -5,13 +7,34 @@ import "@aws-amplify/ui-react/styles.css";
 
 import awsconfig from "./aws-exports.js";
 
+import { UsersLocalStorage } from "./data/local";
+
 Amplify.configure(awsconfig);
 
+const usersLocalStorage = new UsersLocalStorage();
+
 function Root({ signOut, user }) {
-  console.log(user)
+  useEffect(() => {
+    if (!user) return;
+
+    usersLocalStorage.save({
+      id: "current_user",
+      user: {
+        attributes: user.attributes,
+      },
+    });
+}, [user]);
+
   return (<section>
-    <h1>Hello {user.username}</h1>
-    <button onClick={signOut}>Sign out</button>
+    <h1>Hello {user.attributes.email}</h1>
+    <p>Links: Designs | Teams | Settings</p>
+    <button onClick={(e) => {
+      if (!user) return;
+
+      usersLocalStorage.delete("current_user");
+
+      signOut(e);
+    }}>Sign out</button>
   </section>);
 }
 
