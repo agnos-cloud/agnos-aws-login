17f2c8137c94b7fb9fcd06142338f34092b37afa
chieze-franklin <chieze.franklin@gmail.com>
2022-03-20 01:52:50 +0100

    implement aws amplify + cognito

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "agnosAmplifyCognito": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,18 @@
-export default function Root(props) {
-  return <section>AWS Login Component</section>;
+import Amplify from "aws-amplify";
+
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+import awsconfig from "./aws-exports.js";
+
+Amplify.configure(awsconfig);
+
+function Root({ signOut, user }) {
+  console.log(user)
+  return (<section>
+    <h1>Hello {user.username}</h1>
+    <button onClick={signOut}>Sign out</button>
+  </section>);
 }
+
+export default withAuthenticator(Root);
